e720b882a22b64d83244ab8f172f0695773e4826
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-07-29 12:21:53 +0530

    create codesnap

--- CodeSnap/codesnap.py
+++ CodeSnap/codesnap.py
@@ -4,4 +4,36 @@ from PIL import Image
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 class CodeSnap:
-  return None
+  def __init__(self, image_path):
+
+    self.image_path = image_path
+  
+  def loadmodel(self):
+    
+    tokenizer = AutoTokenizer.from_pretrained("usvsnsp/code-vs-nl")
+    model = AutoModelForSequenceClassification.from_pretrained("usvsnsp/code-vs-nl")
+
+    return tokenizer, model
+
+  def retrievecode(self):
+
+    ocr_list = [x for x in pytesseract.image_to_string(self.image_path).split("\n") if x != '']
+    
+    tokenizer, model = self.loadmodel()
+
+    ## Retrieve Code Blocks
+    text_list = []
+    for text in ocr_list:
+      input_ids = tokenizer(text, return_tensors="pt")
+      with torch.no_grad():
+          logits = model(**input_ids).logits
+
+      predicted_class_id = logits.argmax().item()
+
+      if model.config.id2label[predicted_class_id] == "Code":
+        text_list.append(text)
+      
+    
+    return ('\n').join(text_list)
+
+
