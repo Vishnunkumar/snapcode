1a396cbfbc9ef09d7986fbe5f8898ebb8630145d
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-08-05 11:23:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setuptools.setup(
     name="snapcode",
-    version="0.0.2",
+    version="0.0.3",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Extract code blocks from screenshots and images",
